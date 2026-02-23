7f5e906c6fc4b70c4eef80dfc29ebd71fe428ce1
Morthimer McMare <morthimer_second@list.ru>
2026-02-23 21:47:26 +0300

    Fixed alias output format; fixed aliases for `+action`/`-action` commands.

--- manpages/manpages-wiki-autosync-create.sh
+++ manpages/manpages-wiki-autosync-create.sh
@@ -16,7 +16,7 @@ cat "$TEMPFILE" \
 
 # Aliases, "alias ALTNAME CMDNAME":
 cat "$TEMPFILE" \
-	| awk '/^[-+]/ { NM = substr($0, 2); if ( $0 ~ "^+" ) { print "page \t " NM "\tCCMD_" toupper( NM ); } print "alias\t" $0 "\t" NM }' \
+	| awk '/^[-+]/ { NM = substr($0, 2); if ( $0 ~ "^+" ) { print "page \t" $0 "\t CCMD_" toupper( NM ) } else { print "alias\t" $0 "\t +" NM } }' \
 	| column -t --separator "	" --output-separator " " \
 	>> manpages-wiki-autosync.cfg
 
